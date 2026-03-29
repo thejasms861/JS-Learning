ec2c38df311a5bdeebe85ee7a570e44c020344fb
[Enter Name] <[Enter Email]>
2026-03-29 10:18:10 +0530

    Constants and Variables done
    LiveReview Pre-Commit Check: ran (iter:1, coverage:0%)

---
+++ 01_Constants and variables.js
@@ -0,0 +1,25 @@
+//const accountId=12345
+
+//const { is } = require("@react-three/fiber/dist/declarations/src/core/utils")
+
+//var accountName="Thejas"
+
+//let accountEmail="Thejasms914@"
+
+//account="My name is"
+
+//accountEmail=12
+//accountName="sHREYAS"
+//let accounts;
+//console.table([accountEmail,accountName,accountId,accounts])
+
+
+
+//console.log(typeof accountEmail)
+
+
+
+let isLoggedin;
+let conversionisLoggedin= Boolean(isLoggedin)
+console.log(conversionisLoggedin)
+console.log(typeof conversionisLoggedin)
